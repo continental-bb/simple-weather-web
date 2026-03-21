bb4683fa7c85e5f3acc583f10d9fee6619e43664
continental_bbgit config --global user.email <biruktawitsisays@gmail.com>
2026-03-21 15:59:29 +0300

    other repo with mr fuads group -33

---
+++ login/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My React Page!</h1>
+      <p>This is my first simple React app</p>
+    </div>
+  );
+}
+
+export default App;
